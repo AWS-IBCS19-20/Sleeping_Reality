83d3ff5918c4d6b83b448e3a24324556676fdd03
Rashida Francis <rashida_francis@m018130.local>
2019-09-14 12:18:48 -0700

    piazza hw

---
+++ p.java
@@ -0,0 +1,6 @@
+public class p {
+  public static void main(String[] args) {
+    System.out.println("The companies should invest in file recovery software because they are easy targets for cyber attacks.\n They should also invest in antivirus software and a firewall.\n They should do regular backups since they probably can't afford to store data in multiple location");
+  }
+
+}
